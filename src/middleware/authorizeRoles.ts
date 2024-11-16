447ec11c20fe9c006043647a6998a5fa42a89359
Balázs <koltaibalazs1999@gmail.com>
2024-11-16 20:31:01 +0100

    Revert "add console log"
    This reverts commit b4d0158c1de8371ba926f41dd5c4586de5cba4e9.

--- src/middleware/authorizeRoles.ts
+++ src/middleware/authorizeRoles.ts
@@ -2,7 +2,6 @@ import { Request, Response, NextFunction } from "express";
 import { AuthenticatedRequest } from "./authMiddleware";
 
 export const authorizeRoles = (...roles: string[]) => {
-  console.log(roles);
   return (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
     //TODO User is already existing, no need for this
     if (!req.user) {
@@ -12,7 +11,6 @@ export const authorizeRoles = (...roles: string[]) => {
     }
 
     if (!roles.includes(req.user.role)) {
-      console.log(req.user.role);
       return res.status(403).json({
         message: "Forbidden: You do not have access to this resource",
       });
